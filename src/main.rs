a68059638b7dd58b325809e21520842c482c174c
Riley Seaburg <riley@rileyseaburg.com>
2023-09-03 12:34:19 -0400

    feat: Add Linear module and refactor Model structure
    This commit adds a new `Linear` module which contains the weight and bias tensors. The `Model` structure is refactored to use the `Linear` module for its layers. The `forward` methods are updated to call the `forward` method of the `Linear` modules. Additionally, some comments are added to clarify the purpose of certain code blocks.
    
    Inference is performed on the model using a dummy image, and the resulting digit is printed.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,29 @@
 use candle_core::{DType, Device, Result, Tensor};
 
-struct Model {
-    first: Tensor,
-    second: Tensor,
+
+pub struct Linear {
+    weight: Tensor,
+    bias: Tensor,
 }
+
+impl Linear {
+    fn forward(&self, input: &Tensor) -> Result<Tensor> {
+        let x = input.matmul(&self.weight)?;
+        x.broadcast_add(&self.bias)
+    }
+}
+
+pub struct Model {
+    first: Linear,
+    second: Linear,
+}
+
 
 impl Model {
     fn forward(&self, image: &Tensor) -> Result<Tensor> {
-        let x = image.matmul(&self.first)?;
+        let x = self.first.forward(image)?;
         let x = x.relu()?;
-        x.matmul(&self.second)
+        self.second.forward(&x)
     }
 }
 
@@ -20,18 +34,39 @@ fn main() -> Result<()> {
         Err(_) => Device::Cpu,
     };
 
-    let first = Tensor::zeros((784, 100), DType::F32, &device)
+
+
+    // setting first layer weights
+    let weight = Tensor::zeros((784, 100), DType::F32, &device)
         .unwrap()
         .contiguous()?;
-    let second = Tensor::zeros((100, 10), DType::F32, &device)
+
+    // setting first layer bias
+    let bias = Tensor::zeros((100,), DType::F32, &device)
         .unwrap()
         .contiguous()?;
+
+    let first = Linear { weight, bias };
+    
+    // setting second layer weights
+    let weight = Tensor::zeros((100, 10), DType::F32, &device)
+        .unwrap()
+        .contiguous()?;
+
+    // setting second layer bias
+    let bias = Tensor::zeros((10,), DType::F32, &device)
+        .unwrap()
+        .contiguous()?;
+
+    let second = Linear { weight, bias };
+
     let model = Model { first, second };
 
     let dummy_image = Tensor::zeros((1, 784), DType::F32, &device)
         .unwrap()
         .contiguous()?;
 
+    // Inference on the model
     let digit = model.forward(&dummy_image)?;
 
     println!("Digit {digit:?} digit");
