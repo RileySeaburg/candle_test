7ac543f74ebeac8e0632baaca63eaa49485125ca
Riley Seaburg <riley@rileyseaburg.com>
2023-09-03 12:39:14 -0400

    Refactor linear layer and update layer dimensions
    - Refactored the linear layer implementation in order to improve code structure and readability.
    - The dimensions of the first and second layer weights have been changed from (784, 100) to (100, 784) and from (100, 10) to (10, 100) respectively.
    - Fixed indentation and formatting issues.
    - Updated variable names for clarity.
    - Added comments for GPU usage and details.

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,6 @@
 use candle_core::{DType, Device, Result, Tensor};
+use candle_nn::{Linear, Module};
 
-
-pub struct Linear {
-    weight: Tensor,
-    bias: Tensor,
-}
-
-impl Linear {
-    fn forward(&self, input: &Tensor) -> Result<Tensor> {
-        let x = input.matmul(&self.weight)?;
-        x.broadcast_add(&self.bias)
-    }
-}
 
 pub struct Model {
     first: Linear,
@@ -28,48 +17,21 @@ impl Model {
 }
 
 fn main() -> Result<()> {
-    // use Device::new_cuda(0)?; to use the GPU.
-    let device = match Device::new_cuda(0) {
-        Ok(device) => device,
-        Err(_) => Device::Cpu,
-    };
+     // Use Device::new_cuda(0)?; to use the GPU.
+     let device = Device::Cpu;
 
-
-
-    // setting first layer weights
-    let weight = Tensor::zeros((784, 100), DType::F32, &device)
-        .unwrap()
-        .contiguous()?;
-
-    // setting first layer bias
-    let bias = Tensor::zeros((100,), DType::F32, &device)
-        .unwrap()
-        .contiguous()?;
-
-    let first = Linear { weight, bias };
-    
-    // setting second layer weights
-    let weight = Tensor::zeros((100, 10), DType::F32, &device)
-        .unwrap()
-        .contiguous()?;
-
-    // setting second layer bias
-    let bias = Tensor::zeros((10,), DType::F32, &device)
-        .unwrap()
-        .contiguous()?;
-
-    let second = Linear { weight, bias };
-
-    let model = Model { first, second };
-
-    let dummy_image = Tensor::zeros((1, 784), DType::F32, &device)
-        .unwrap()
-        .contiguous()?;
-
-    // Inference on the model
-    let digit = model.forward(&dummy_image)?;
-
-    println!("Digit {digit:?} digit");
-
-    Ok(())
+     // This has changed (784, 100) -> (100, 784) !
+     let weight = Tensor::zeros((100, 784), DType::F32, &device)?;
+     let bias = Tensor::zeros((100, ), DType::F32, &device)?;
+     let first = Linear::new(weight, Some(bias));
+     let weight = Tensor::zeros((10, 100), DType::F32, &device)?;
+     let bias = Tensor::zeros((10, ), DType::F32, &device)?;
+     let second = Linear::new(weight, Some(bias));
+     let model = Model { first, second };
+ 
+     let dummy_image = Tensor::zeros((1, 784), DType::F32, &device)?;
+ 
+     let digit = model.forward(&dummy_image)?;
+     println!("Digit {digit:?} digit");
+     Ok(())
 }
